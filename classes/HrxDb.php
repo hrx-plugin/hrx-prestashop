417c08c32e9e42bd31fc5c57a6632fcb427f8a6d
Rimvydas V <lithstud@gmail.com>
2023-08-08 14:02:21 +0300

    Removed SQL files, sql table queries moved to HrxDb

--- classes/HrxDb.php
+++ classes/HrxDb.php
@@ -3,29 +3,63 @@
 class HrxDB
 {
     const TABLES = [
-        'hrx_order',
-        'hrx_warehouse',
-        'hrx_cart_terminal',
-        'hrx_shipping_price',
+        'hrx_cart_terminal' => "
+            CREATE TABLE IF NOT EXISTS `" . _DB_PREFIX_ . "hrx_cart_terminal` (
+                `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                `id_cart` int(10) NOT NULL,
+                `delivery_location_id` varchar(36) NOT NULL,
+                `date_add` datetime NOT NULL,
+                `date_upd` datetime NOT NULL,
+                PRIMARY KEY (`id`),
+                KEY `id_cart` (`id_cart`)
+            ) ENGINE=" . _MYSQL_ENGINE_ . " DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+        ",
+        'hrx_order' => "
+            CREATE TABLE IF NOT EXISTS `" . _DB_PREFIX_ . "hrx_order` (
+                `id` int(10) unsigned NOT NULL,
+                `id_shop` int(10) NOT NULL,
+                `id_hrx` varchar(36),
+                `pickup_location_id` varchar(36) NOT NULL,
+                `terminal` varchar(255),
+                `delivery_location_id` varchar(36) NOT NULL,
+                `length` float(10) NOT NULL,
+                `width` float(10) NOT NULL,
+                `height` float(10) NOT NULL,
+                `weight` float(10) NOT NULL,
+                `tracking_number` varchar(32),
+                `tracking_url` varchar(100),
+                `status` int(10) NOT NULL,
+                `status_code` varchar(15) NOT NULL,
+                `kind` varchar(17) NOT NULL,
+                `date_add` datetime NOT NULL,
+                `date_upd` datetime NOT NULL,
+                PRIMARY KEY (`id`),
+                KEY `id_shop` (`id_shop`)
+            ) ENGINE=" . _MYSQL_ENGINE_ . " DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+        ",
+        'hrx_warehouse' => "
+            CREATE TABLE IF NOT EXISTS `" . _DB_PREFIX_ . "hrx_warehouse` (
+                `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                `id_warehouse` varchar(36) NOT NULL,
+                `name` varchar(60) NOT NULL,
+                `country` varchar(2),
+                `city` varchar(40) NOT NULL,
+                `address` varchar(50) NOT NULL,
+                `zip` varchar(6) NOT NULL,
+                `default_warehouse` tinyint NOT NULL,
+                `date_add` datetime NOT NULL,
+                `date_upd` datetime NOT NULL,
+                PRIMARY KEY (`id`)
+            ) ENGINE=" . _MYSQL_ENGINE_ . " DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+        ",
     ];
+
     /**
      * Create tables for module
      */
     public function createTables()
     {
-        $sql_path = dirname(__FILE__) . '/../sql/';
-        $sql_files = scandir($sql_path);
-        $sql_queries = [];
-        foreach($sql_files as $sql_file)
-        {
-            $file_parts = pathinfo($sql_file);
-            if($file_parts['extension'] == 'sql')
-            {
-                $sql_query = str_replace('_DB_PREFIX_', _DB_PREFIX_, Tools::file_get_contents($sql_path . $sql_file));
-                $sql_queries[] = str_replace('_MYSQL_ENGINE_', _MYSQL_ENGINE_, $sql_query);
-            }
-        }
-        foreach ($sql_queries as $query) {
+        foreach (self::TABLES as $table => $query) {
             try {
                 $res_query = Db::getInstance()->execute($query);
 
@@ -37,6 +71,7 @@ class HrxDB
             }
         }
 
+        // Added in v1.2.0
         try {
             HrxDeliveryCourier::addSqlTable();
             HrxDeliveryTerminal::addSqlTable();
@@ -52,13 +87,14 @@ class HrxDB
      */
     public function deleteTables()
     {
-        foreach (self::TABLES as $table) {
+        foreach (array_keys(self::TABLES) as $table) {
             try {
                 Db::getInstance()->execute("DROP TABLE IF EXISTS " . _DB_PREFIX_ . $table);
             } catch (Exception $e) {
             }
         }
 
+        // added in v1.2.0
         try {
             HrxDeliveryCourier::dropSqlTable();
             HrxDeliveryTerminal::dropSqlTable();
